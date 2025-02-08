af59699ac54e7442c5c286f2f3adbfc017657eb5
Tyler Simile <tylerjsimile@gmail.com>
2025-02-08 15:32:55 -0500

    working with print statments

--- stl-to-vox.py
+++ stl-to-vox.py
@@ -5,13 +5,17 @@ import os
 
 tensor_list = []
 
-directory = "stls/"
+directory = "stls"
+
+count = 1
 
 for i in os.listdir(directory):
     if i.endswith(".stl"):
+        print(count)
+        count = count + 1
         file_path = os.path.join(directory, i)
         mesh = trimesh.load_mesh(file_path)
-        voxelized = trimesh.voxel.creation.voxelize(mesh, pitch=0.5)
+        voxelized = trimesh.voxel.creation.voxelize(mesh, pitch=1)
         voxel_array = voxelized.matrix
         tensor = torch.from_numpy(voxel_array)
         tensor_list.append(tensor)
