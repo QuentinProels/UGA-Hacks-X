322e0b3940c831e79299a3d07383e720ec3bdc8b
Quentin Proels <quentin1589@live.com>
2025-02-08 17:22:20 -0500

    vox visualiser

--- vox_visualizer.py
+++ vox_visualizer.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import trimesh
 
-voxel_grid = np.load("dataset/voxel_1.npy")
+voxel_grid = np.load("dataset/voxel_36.npy")
 
 vox_mesh = trimesh.voxel.VoxelGrid(voxel_grid)
 mesh = vox_mesh.as_boxes()
