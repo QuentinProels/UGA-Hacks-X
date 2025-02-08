571cd4211e9aff27abf1e78d806b1b3e019f2e3c
Quentin Proels <quentin1589@live.com>
2025-02-08 16:15:45 -0500

    added data set and changed stl-to-vox to actually stl-to-vox

--- stl-to-vox.py
+++ stl-to-vox.py
@@ -3,17 +3,20 @@ import numpy as np
 import torch
 import os
 
-tensor_list = []
 
-directory = "stls/"
+directory = "stls"
+count = 1
 
 for i in os.listdir(directory):
     if i.endswith(".stl"):
+        print("ran")
         file_path = os.path.join(directory, i)
         mesh = trimesh.load_mesh(file_path)
-        voxelized = trimesh.voxel.creation.voxelize(mesh, pitch=0.5)
+        voxelized = trimesh.voxel.creation.voxelize(mesh, pitch=1)
         voxel_array = voxelized.matrix
-        tensor = torch.from_numpy(voxel_array)
-        tensor_list.append(tensor)
+        np.save(f"dataset/voxel_{count}.npy", voxel_array)
+        count = count+1        
+
+
            
     
