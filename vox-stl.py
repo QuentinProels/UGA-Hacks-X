b180bc313f20df9b2d555f1c5b7ee9960b1667c5
Tyler Simile <tylerjsimile@gmail.com>
2025-02-09 03:05:27 -0500

    added comments

--- vox-stl.py
+++ vox-stl.py
@@ -2,15 +2,15 @@ import numpy as np
 import trimesh
 from skimage.measure import marching_cubes
 import pyvista
-
+#loads the mesh
 voxel_grid = np.load("dataset/voxel_36.npy")
-
+#applies marching cubes algoithm to the mesh
 vertices, faces, _, _ = marching_cubes(voxel_grid, level=0.5)
 
 mesh = trimesh.Trimesh(vertices=vertices,faces=faces)
 
 pv_mesh = pyvista.wrap(mesh)
-
+#smooths and outputs mesh to a test file
 smooth = pv_mesh.smooth(n_iter=100, relaxation_factor=0.1)
 
 smooth.save("outputs/outputTest.stl")
--- vox_visualizer.py
+++ vox_visualizer.py
@@ -2,7 +2,7 @@ import numpy as np
 import trimesh
 
 voxel_grid = np.load("dataset/voxel_36.npy")
-
+#loads the voxel mesh for visualization
 vox_mesh = trimesh.voxel.VoxelGrid(voxel_grid)
 mesh = vox_mesh.as_boxes()
 mesh.show()
